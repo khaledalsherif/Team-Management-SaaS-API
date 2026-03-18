b8d05bde00b2189fa23a583aad5355554b34811a
devKhaled <khaled7740774@gmail.com>
2026-03-18 07:12:30 +0200

    Clean up restrictToTeam by removing unused code
    Removed commented-out permission checks for clarity.

--- src/utils/restrictToTeam.js
+++ src/utils/restrictToTeam.js
@@ -1,12 +1,5 @@
 import AppError from './appError';
 export const restrictToTeam = (roleInTeam, ...roles) => {
-  // const managerPermision = roles.includes('MANAGER');
-  // const isOwner = team.ownerId === id;
-  // const isManager = managerPermision
-  //   ? team.teamMembers.some((member) => {
-  //       return member.userId === id && member.role === 'MANAGER';
-  //     })
-  //   : false;
   const hasPermision = roles.includes(roleInTeam);
   if (!hasPermision) {
     throw new AppError(
